033813701b3a3f1bf1cc072ba392841e9248247b
chotmp <chotmp@gmail.com>
2014-04-15 17:41:34 -0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'efb11504a606caf49eadf5e5eef7f72a9b28b902b8e02abfd190157df8daff1deab427ed3cf11dc98ecc2db995e6f6b89e09aa3247a19f7b837704170bb4013e'
